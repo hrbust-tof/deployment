aaa31e75edb8aca90a66365f9c4bf9bd3f46a46e
wangsong <1057864096@qq.com>
2019-08-05 20:53:10 +0800

    add markDown

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# deployment
+# custom
+## 接口文档
+---
+#### 测试post
+##### url:test/post
+##### method:POST
+##### body
+```json
+{
+	"data":"String"
+}
+```
+
+---
+#### 测试get
+##### url:test/get
+##### method:GET
+
--- src/main/java/com/tof/deployment/controller/TestController.java
+++ src/main/java/com/tof/deployment/controller/TestController.java
@@ -5,6 +5,7 @@ import com.tof.deployment.dto.BaseResult;
 import com.tof.deployment.dto.test.TestPost;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,13 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class TestController {
     @ForMD("测试get")
-    @RequestMapping("get")
+    @RequestMapping(value = "get", method = RequestMethod.GET)
     public BaseResult testGet() {
         return BaseResult.getOkBaseResult();
     }
 
     @ForMD("测试post")
-    @RequestMapping("post")
+    @RequestMapping(value = "post", method = RequestMethod.POST)
     public BaseResult testPost(@RequestBody TestPost.Arg arg) {
         return new TestPost.Result(arg.getData()).setOk();
     }
